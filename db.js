98e790a23c999391387c8dc9d1747033588d5901
Matthew Eckes <eckesm@gmail.com>
2021-08-12 11:33:44 -0400

    Completed Step 2: Add Company Routes.

--- db.js
+++ db.js
@@ -1,2 +1,20 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+let DB_URI;
+
+if (process.env.NODE_ENV === 'test') {
+	DB_URI = 'postgres:///biztime_test';
+}
+else {
+	DB_URI = 'postgres:///biztime';
+}
+
+let db = new Client({
+	connectionString : DB_URI
+});
+
+db.connect();
+
+module.exports = db;
